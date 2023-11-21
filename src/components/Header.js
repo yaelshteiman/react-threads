f0b684ee83d5841847e03d3a74f1361901bf5438
yaelshteiman <yaelshtaiman@gmail.com>
2023-11-21 12:08:38 +0200

    adding div and button elements to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,29 @@
-
 const Header = () => {
     return (
-        <heade>
-        </heade>
+        <header>
+            <div className="info-container">
+                <div className="user-info-container">
+                    <h1>username</h1>
+                    <p>handle <span className="threads-info">threads.net</span></p>
+                </div>
+                <div className="img-container">
+                    <img src="" alt="profile avatar"/>
+                </div>
+            </div>
+            <p>bio</p>
+            <div className="sub-info-container">
+                <p className="sub-text">X followers • <a href="">link</a></p>
+            </div>
+            <button
+                className="primary"
+                onClick={() => navigator.clipboard.writeText('I am a URL')}>
+                Share Profile
+            </button>
+            <div className="buttom-container">
+                <button>Threads</button>
+                <button>Replies</button>
+            </div>
+        </header>
     )
 }
 
